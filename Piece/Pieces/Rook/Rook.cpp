0469bfea4813f2c9f75a44f8897197655c52d060
Yam <yamyam610@gmail.com>
2023-06-14 17:44:28 +0300

    Implementing Pieces

---
+++ Piece/Pieces/Rook/Rook.cpp
@@ -0,0 +1,17 @@
+//
+// Created by Yam on 14/06/2023.
+//
+
+#include "Pawn.h"
+
+
+std::vector<Move> getAllPossibleMoves() {
+    // TODO: Implement
+    return std::vector<Move>();
+}
+
+std::string toString() {
+    std::string piece_string = "";
+    piece_string += PAWN_SYMBOL;
+    return piece_string;
+}
